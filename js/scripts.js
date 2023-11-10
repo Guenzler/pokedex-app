d3b2698280eb527685d83eb9379336ce12b69ccf
eva2526 <148702094+eva2526@users.noreply.github.com>
2023-11-10 13:14:21 +0100

    display pokemons on website
    for each pokemon, display all its properties,
    check, whether pokemon is bigger than 70 cm

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// define three pokemons
 let pokemon1 = {
     name: 'Ivysaur',
     height: 100,
@@ -19,4 +20,34 @@ let pokemon3 = {
     abilities: ['Keen-eye', 'Sniper']
 };
 
+// define list of pokemons
 let pokemonList = [pokemon1, pokemon2, pokemon3];
+
+//write names and properties of all pokemons in websites DOM
+for (let i = 0; i < pokemonList.length; i++) {
+
+    //write name of pokemon as headline
+    document.write(`<h2>${pokemonList[i].name}</h2>`);
+
+    //in the following paragraph display all properties of pokemon
+    document.write(`<p><span class="highlight">Properties of ${pokemonList[i].name}:</span><br>`);
+    document.write(`<span class="highlight">Height:</span> ${pokemonList[i].height} cm`);
+
+    //determine if this is a big pokemon
+    if (pokemonList[i].height > 70){
+        document.write(`<span class="exclaim"> - Wow, that's big!</span>`);
+    }
+
+    //display all types of this pokemon
+    document.write(`<br><span class="highlight">Types:</span> `);
+    for (let j = 0; j < pokemonList[i].types.length; j++){
+        document.write(`${pokemonList[i].types[j]} `);
+    }
+
+    //display all abilities of pokemon
+    document.write(`<br><span class="highlight">Abilities:</span> `);
+    for (let k = 0; k < pokemonList[i].abilities.length; k++){
+        document.write(`${pokemonList[i].abilities[k]} `);
+    }
+    document.write(`</p>`);
+}
